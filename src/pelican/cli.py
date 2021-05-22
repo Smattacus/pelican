d8fe817459979d9213cd095e74ae42af71dd47ea
Smattacus <Smattacus@gmail.com>
2021-05-22 16:49:16 -0500

    Teach a pelican how to fish.

--- src/pelican/cli.py
+++ src/pelican/cli.py
@@ -1,1 +1,9 @@
 import click
+
+@click.command()
+@click.argument('name')
+def hello(name):
+   click.echo(f'Hello {name}!')
+
+def catch_fish(fish_type):
+    print(f"I've got a {fish_type}!")
---
+++ src/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "project pelican",
+    version = "0.1",
+    author = "Sean Mattingly",
+    author_email = "Smattacus@gmail.com",
+    description = ("Simple github runner stuff."),
+    license = "none",
+    keywords = "example documentation tutorial",
+    url = "",
+    packages=['pelican'],
+    long_description='',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    requires=[
+        "click",
+    ]
+)
